a5a7ae9c60a131abda44c1452228e032a4b39a9c
Ashish Bhardwaj <erashish41@gmail.com>
2024-08-10 14:19:34 +0530

    Todo List component added

--- 02_todo_app/src/components/TodoList.jsx
+++ 02_todo_app/src/components/TodoList.jsx
@@ -2,6 +2,7 @@ import { MdCheck, MdDeleteForever } from "react-icons/md"
 
 export const TodoList = ({key, data, onHandleDeleteTodo}) => {
     return (
+        
         <li key={key} className="todo-item">
             <span>{data}</span>
             <button className="check-btn">
