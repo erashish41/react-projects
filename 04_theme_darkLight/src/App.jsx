55fc4b8f5614c624008c085c82377921f9eba873
Ashish Bhardwaj <erashish41@gmail.com>
2024-08-22 23:58:19 +0530

    dark light mode

---
+++ 04_theme_darkLight/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Darklight, ThemeProvider } from './Darklight'
+
+function App() {
+
+
+  return (
+    <>
+      <ThemeProvider>
+        <Darklight/>
+      </ThemeProvider>
+    </>
+  )
+}
+
+export default App
---
+++ 04_theme_darkLight/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line if your files are located elsewhere
+  ],
+  theme: {
+    extend: {},
+    fontFamily:{
+      display: ["Syne"],
+    },
+  },
+  plugins: [],
+}
