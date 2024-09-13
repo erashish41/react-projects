c51aca65639cea2ab9860d1626f405e5e3e11d21
Ashish Bhardwaj <erashish41@gmail.com>
2024-09-13 20:02:54 +0530

    Image slider

---
+++ 08_image_slider/src/App.jsx
@@ -0,0 +1,11 @@
+import { ImageSlider } from "./components/ImageSlider";
+
+const App = () => {
+  return (
+    <>
+      <ImageSlider />
+    </>
+  )
+}
+
+export default App;
---
+++ 08_image_slider/src/components/ImageSlider.jsx
@@ -0,0 +1,7 @@
+export const ImageSlider = () => {
+    return (
+        <div className="container">
+            <h2>ImageSlider</h2>
+        </div>
+    )
+}
